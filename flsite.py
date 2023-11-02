22af33e90e150b36417be96f7fb24aa0e63dc5aa
Александра Бабкина <73515984+Balexandrina@users.noreply.github.com>
2023-11-02 19:55:11 +0300

    Update flsite.py

--- flsite.py
+++ flsite.py
@@ -1,42 +1,44 @@
 import sqlite3
 import os
+from turtle import title
 from flask import Flask, render_template, request, g, flash, abort
 from FDataBase import FDataBase
 
-DATABASE = 'flsite.db'
+DATABASE = 'site.db'
 DEBUG = True
 SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
-
+ 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-app.config.update(dict(DATABASE=os.path.join(app.root_path,'flsite.db')))
+app.config.update(dict(DATABASE=os.path.join(app.root_path,'site.db')))
 
 def connect_db():
     conn = sqlite3.connect(app.config['DATABASE'])
-    conn.row_factory = sqlite3.Row
+    conn.row_factory = sqlite3.Row 
     return conn 
 
 def create_db():
-    """Вспомогательная функция для создания таблиц БД"""
     db = connect_db()
-    with app.open_resource('sq_db.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
+    db.execute('CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL)') # таблица для постов блога
+    db.execute('CREATE TABLE IF NOT EXISTS review (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, text TEXT NOT NULL, time integer NOT NULL)') # таблица для отзывов покупателей
     db.commit()
     db.close()
 
-def get_db():
-    '''Соединение с БД, если оно еще не установлено'''
+def get_db(): # Соединение с БД, если оно еще не установлено
     if not hasattr(g, 'link_db'):
         g.link_db = connect_db()
     return g.link_db
 
 @app.teardown_appcontext
-def close_db(error):
-    '''Закрываем соединение с БД, если оно было установлено'''
+def close_db(error): # Закрываем соединение с БД, если оно было установлено
     if hasattr(g, 'link_db'):
         g.link_db.close()
 
+@app.before_first_request
+def before_first_request():
+    create_db()
+    
 @app.route("/")
 def index():
     db = get_db()
@@ -56,9 +58,9 @@ def addReview():
     db = get_db()
     dbase = FDataBase(db)
  
-    if request.method == "POST":
-        if len(request.form['name']) > 2 and len(request.form['post']) > 2: # basic
-            res = dbase.addReview(request.form['name'], request.form['post']) 
+    if request.method == "POST": # если данные от формы пришли
+        if len(request.form['name']) > 2 and len(request.form['post']) > 2: # примитивная проверка на количсетво симолов в заголовке и самой статье
+            res = dbase.addReview(request.form['name'], request.form['post']) # , request.form['url'])
             if not res:
                 flash('Ошибка добавления отзыва', category = 'error')
             else:
@@ -68,14 +70,20 @@ def addReview():
  
     return render_template('add_feedback.html', menu = dbase.getMenu(), title="Отзывы", posts = dbase.getPostsAnonce()) 
 
-@app.route("/post/<alias>")
-def showPost(alias):
+@app.route('/blog')
+def blog():
     db = get_db()
-    dbase = FDataBase(db)
-    title, post = dbase.getPost(alias)
-    if not title:
-        abort(404)
-    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
+    posts = db.execute('SELECT * FROM posts').fetchall()
+    db.close()
+    return render_template('blog.html', posts = posts)
+
+@app.route('/<int:post_id>')
+def get_post(post_id):
+    db = get_db()
+    db.execute('INSERT INTO posts (title, content) VALUES ("Random Title", "Lorem ipsum dolor sit amet consectetur adipiscing elit")')
+    post = db.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    db.close()
+    return render_template('post.html', post=post)
 
 if __name__ == '__main__':
     app.run(debug=True)
