0305afb7badf128f5cb84a2ec91c6297f67c6461
Александра Бабкина <73515984+Balexandrina@users.noreply.github.com>
2023-11-02 19:55:44 +0300

    Update FDataBase.py

--- FDataBase.py
+++ FDataBase.py
@@ -10,7 +10,7 @@ class FDataBase:
         self.__cur = db.cursor()
  
     def getMenu(self):
-        sql = '''SELECT * FROM mainmenu'''
+        sql = '''SELECT * FROM review'''
         try:
             self.__cur.execute(sql)
             res = self.__cur.fetchall()
@@ -22,7 +22,7 @@ class FDataBase:
     def addReview(self, title, text):
         try:
             tm = math.floor(time.time())
-            self.__cur.execute("INSERT INTO posts VALUES(NULL, ?, ?, ?)", (title, text, tm))
+            self.__cur.execute("INSERT INTO review VALUES(NULL, ?, ?, ?)", (title, text, tm))
             self.__db.commit()
         except sqlite3.Error as e:
             print("Ошибка добавления статьи в БД "+str(e))
@@ -32,7 +32,7 @@ class FDataBase:
     
     def getPost(self, alias):
         try:
-            self.__cur.execute("SELECT title, text FROM posts WHERE url LIKE ? LIMIT 1", (alias,))
+            self.__cur.execute("SELECT title, text FROM review WHERE url LIKE ? LIMIT 1", (alias,))
             res = self.__cur.fetchone()
             if res:
                 base = url_for('static', filename='images_html')
@@ -47,7 +47,7 @@ class FDataBase:
     
     def getPostsAnonce(self):
         try:
-            self.__cur.execute(f"SELECT id, title, text FROM posts ORDER BY time DESC")
+            self.__cur.execute(f"SELECT id, title, text FROM review ORDER BY time DESC")
             res = self.__cur.fetchall()
             if res: return res
         except sqlite3.Error as e:
