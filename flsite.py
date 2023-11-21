defe80582ae22f73e84556427994c837567c4779
Александра Бабкина <73515984+Balexandrina@users.noreply.github.com>
2023-11-21 19:17:20 +0300

    Update flsite.py

--- flsite.py
+++ flsite.py
@@ -3,11 +3,12 @@ import os
 from turtle import title
 from flask import Flask, render_template, request, g, flash, abort
 from FDataBase import FDataBase
+from BlogDB import BlogDataBase
 
 DATABASE = 'site.db'
 DEBUG = True
-SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
- 
+SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'  
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 
@@ -15,15 +16,21 @@ app.config.update(dict(DATABASE=os.path.join(app.root_path,'site.db')))
 
 def connect_db():
     conn = sqlite3.connect(app.config['DATABASE'])
-    conn.row_factory = sqlite3.Row 
+    conn.row_factory = sqlite3.Row
     return conn 
 
 def create_db():
     db = connect_db()
-    db.execute('CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL)') # таблица для постов блога
-    db.execute('CREATE TABLE IF NOT EXISTS review (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, text TEXT NOT NULL, time integer NOT NULL)') # таблица для отзывов покупателей
-    db.commit()
-    db.close()
+    db.execute('CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL, img BLOB NOT NULL, img_name TEXT NOT NULL)')
+    db.execute('CREATE TABLE IF NOT EXISTS review (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, text TEXT NOT NULL, time integer NOT NULL)')
+
+# функция для вставки постов в блог
+def insert_posts():
+    db = connect_db()
+    dbase = BlogDataBase(db)
+    dbase.insert_blob(1, 'Итоги выставки "Сибирская продовольственная неделя"', 'Итак: итоги выставки "Сибирская продовольственная неделя", для нашей мастерской, оказались позитивными.', "novosib.jpg", "novosib.jpg")
+    dbase.insert_blob(2, "Линейка шоколада «Мой Новосибирск»", "Запущена в разработку линейка шоколада «Мой Новосибирск», состоящая из пяти плиток с рельефными изображениями живописных видов Новосибирска." ,"blog_novosib.jpg", "blog_novosib.jpg")
+    dbase.insert_blob(3, "Выставка в Екатеринбурге", "В ноябре 2021 года наша компания участвовала в в международной выставке InterFoodUral2021 в составе делегации Кузбасса." , "main.jpg", "main.jpg")   
 
 def get_db(): # Соединение с БД, если оно еще не установлено
     if not hasattr(g, 'link_db'):
@@ -38,12 +45,10 @@ def close_db(error): # Закрываем соединение с БД, если
 @app.before_first_request
 def before_first_request():
     create_db()
-    
+
 @app.route("/")
 def index():
-    db = get_db()
-    dbase = FDataBase(db)
-    return render_template('index.html', title='Главная', menu = dbase.getMenu())
+    return render_template('index.html', title='Главная')
 
 @app.route('/about')
 def about():
@@ -59,8 +64,8 @@ def addReview():
     dbase = FDataBase(db)
  
     if request.method == "POST": # если данные от формы пришли
-        if len(request.form['name']) > 2 and len(request.form['post']) > 2: # примитивная проверка на количсетво симолов в заголовке и самой статье
-            res = dbase.addReview(request.form['name'], request.form['post']) # , request.form['url'])
+        if len(request.form['name']) > 2 and len(request.form['post']) > 2: # простая проверка на количество символов в имени и отзыве
+            res = dbase.addReview(request.form['name'], request.form['post'])
             if not res:
                 flash('Ошибка добавления отзыва', category = 'error')
             else:
@@ -72,7 +77,7 @@ def addReview():
 
 @app.route('/blog')
 def blog():
-    db = get_db()
+    db = connect_db()
     posts = db.execute('SELECT * FROM posts').fetchall()
     db.close()
     return render_template('blog.html', title='Блог', posts = posts)
@@ -80,11 +85,10 @@ def blog():
 @app.route('/<int:post_id>')
 def get_post(post_id):
     db = get_db()
-    db.execute('INSERT INTO posts (title, content) VALUES ("Random Title", "Lorem ipsum dolor sit amet consectetur adipiscing elit")')
     post = db.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
     db.close()
-    return render_template('post.html', post=post)
- 
+    return render_template('post.html', post=post) # рендерим HTML-шаблон и передаём туда полученный пост
+
 @app.errorhandler(404)
 def pageNotFount(error):
     return render_template('page404.html', title="Страница не найдена")
@@ -93,3 +97,4 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 # create_db()
+# insert_posts()
